fe0c44162634f0aba84c63284b1c08135389b129
Ryan Kwast <15rek@queensu.ca>
2017-11-17 00:11:18 -0500

    Basic server
    -Updated package.json file to include express and body parser dependencies
    -Created first get/post request functions in server code

--- server/Express Server.js
+++ server/Express Server.js
@@ -4,8 +4,28 @@ var bp = require('body parser');
 
 app = express();
 
-app.get('/', function(req, res){
-    console.log(req);
-    console.log(res);
-    res.sendFile(path.join(__dirname, 'demo.html')
-};
+const PORT = 4000;
+
+// gives the user the signup html page at the domain home path
+// Must figure out how to include css part
+app.get('/', function(req, res){ //why is there an error in the get here, but not in line 19?
+    /* To add: include conditional statement to check if user has already signed in,
+    in which case redirect to 'default' page */
+    res.redirect('/SignUp');
+});
+
+/* gives the user the signup html page at the domain home path
+ Must figure out how to include css part*/
+app.get('/SignUp', function(req, res){
+    res.sendFile(path.join('__dirname', '..', 'client', 'SignupNew.html');
+});
+
+app.post('/NewUser', function(req, res)  {
+    var jsondata;
+    var new_user = user()
+});
+
+
+// start the server
+app.listen(PORT);
+console.log('Listening on port ' + 'PORT'); //why does this not get logged when running the program?
