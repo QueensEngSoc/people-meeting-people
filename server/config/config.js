42eee4364d3026884a4a1cb84e2f9204d0505357
Astral Cai <14qc5@queensu.ca>
2017-11-18 11:10:47 -0500

    config file

---
+++ server/config/config.js
@@ -0,0 +1,14 @@
+const config = {
+    'host': 'localhost',
+    'port': 4000,
+    'db': {
+        database: 'test',
+        username: 'root',
+        password: 'differential_equations',
+        host: 'localhost',
+        port: 3306,
+        connectionLimit: 10
+    }
+};
+
+module.exports = config;
