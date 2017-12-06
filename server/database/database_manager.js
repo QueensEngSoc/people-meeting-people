8c7d12857a5c21118003ad502988d2a41faa088a
Astral Cai <14qc5@queensu.ca>
2017-12-05 23:39:49 -0500

    User class created, createUser method implemented

--- server/database/database_manager.js
+++ server/database/database_manager.js
@@ -21,19 +21,39 @@ class DatabaseManager {
     constructor() {
         this.connection_ = new Sequelize(config.db_config[lit.DB_DATABASE],
             config.db_config[lit.DB_USERNAME], config.db_config[lit.DB_PASSWORD], {
-            host: config.db_config[lit.DB_HOST],
-            port: config.db_config[lit.DB_PORT],
-            pool: {
-                max: config.db_config[lit.DB_CONNECTION_LIMIT]
-            },
-            dialect: 'mysql',
-            operatorsAliases: false
-        });
+                host: config.db_config[lit.DB_HOST],
+                port: config.db_config[lit.DB_PORT],
+                pool: {
+                    max: config.db_config[lit.DB_CONNECTION_LIMIT]
+                },
+                dialect: 'mysql',
+                operatorsAliases: false
+            });
         let thisManager = this;
         Object.keys(tables).forEach((key) => {
             let table = tables[key];
             let con = thisManager.connection_;
             thisManager[table.table_name] = con.define(table.table_name, table.fields);
+        });
+    }
+
+    /**
+     * Creates a new user and saves it to the database
+     * @param {Object} values Basic user information required to create a new user
+     * @return {Promise<User|DatabaseError>} resolves a User object
+     */
+    createUser(values) {
+        return User.createUser(values, this[lit.tables.USERS]);
+    }
+
+    /**
+     * checks if a user with a provided netID exist in the database
+     * @param userID
+     * @returns boolean
+     */
+    existsUser(userID) {
+        this[lit.tables.USERS].findById(userID).then((result) => {
+            return result != null;
         });
     }
 
--- server/database/user.js
+++ server/database/user.js
@@ -7,13 +7,28 @@
 
 const Sequelize = require('sequelize');
 const lit = require('../utilities/literals');
+const errors = require('../utilities/error');
 
 /**
  * The interface object for functionality relating to users
  */
 class User {
-    constructor(args, model) {
-        
+    constructor(instance) {
+        this.instance_ = instance
+    }
+
+    static createUser(values, model) {
+        return new Promise((resolve, reject) => {
+            model.findById(values[lit.fields.USER_ID]).then((result) => {
+                if (result == null) reject(
+                    new errors.DuplicateEntryError('This user already exists in the database'));
+                return model.create(values);
+            }).then((instance) => {
+                resolve(new User(instance));
+            }).catch((error) => {
+                reject(new errors.FailedQueryError(error.message));
+            });
+        });
     }
 }
 
