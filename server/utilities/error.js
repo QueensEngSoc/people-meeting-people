1cdddab20e3e8223332f2deddab4740cb88303e1
Astral Cai <14qc5@queensu.ca>
2017-12-05 23:38:01 -0500

    add custom error classes

---
+++ server/utilities/error.js
@@ -0,0 +1,46 @@
+/**
+ * @fileoverview custom error classes for Queen's Housing Connect
+ * @author astral.cai@queensu.ca (Astral Cai)
+ */
+
+"use strict";
+
+class BaseError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = 'BaseError';
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+exports.BaseError = BaseError;
+
+class DatabaseError extends BaseError {
+    constructor(message) {
+        super(message);
+        this.name = 'DatabaseError';
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+exports.DatabaseError = DatabaseError;
+
+class FailedQueryError extends DatabaseError {
+    constructor(message) {
+        super(message);
+        this.name = 'FailedQueryError';
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+exports.FailedQueryError = FailedQueryError;
+
+class DuplicateEntryError extends DatabaseError {
+    constructor(message) {
+        super(message);
+        this.name = 'DuplicateEntryError';
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+exports.DuplicateEntryError = DuplicateEntryError;
