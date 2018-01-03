c01b7b0ed49959c116c302859656e848026f99ad
Astral Cai <astral.cai@queensu.ca>
2018-01-03 22:28:06 +0800

    cleaner and more comprehensive unit tests

--- test/unit/test_dbm_group_creation.js
+++ test/unit/test_dbm_group_creation.js
@@ -1,22 +1,22 @@
-const expect = require('chai').expect;
+const chai = require("chai");
+const expect = chai.expect;
 const DatabaseManager = require('../../server/database');
 const lit = require('../../server/utilities/literals');
 const err = require('../../server/utilities/error');
 
 describe('function to create a new housing group', function () {
     let dbm = new DatabaseManager();
+    let testUser = {};
     before(() => {
-        return dbm.createUser({netId: '12ab3', name: 'Test User', email: '12ab3@queensu.ca'});
+        return dbm.createUser({netId: '12ab3', name: 'Test User', email: '12ab3@queensu.ca'}).then(user => {
+            testUser = user;
+        })
     });
     it('should create a new group without error', function () {
-        return dbm.getUser('12ab3').then(user => {
-            return dbm.createGroup(user, 4);
-        });
+        return dbm.createGroup(testUser, 4);
     });
     it('should have created a group with correct basic info', function () {
-        return dbm.getUser('12ab3').then(user => {
-            return user.instance_.getHousingGroup();
-        }).then(group => {
+        return testUser.instance_.getHousingGroup().then(group => {
             expect(group.get('size')).to.equal(4);
             expect(group.get('spotsLeft')).to.equal(3);
             return group.getInitiator();
@@ -25,11 +25,9 @@ describe('function to create a new housing group', function () {
         });
     });
     after(() => {
-        return dbm.getUser('12ab3').then(user => {
-            return user.instance_.getHousingGroup().then(group => {
-                group.destroy();
-                return user.instance_.destroy();
-            });
+        testUser.instance_.getHousingGroup().then(group => {
+            group.destroy();
+            return testUser.instance_.destroy();
         });
     });
 });
