6765113b2473dcd23a30c004430a1b7c1e33a6cf
Ryan Kwast <15rek@queensu.ca>
2018-02-04 16:54:55 -0500

    Created directories for handlebars
    Set up the project to be able to use handlebars. Still need to convert pages--ideally done by next weekend.

--- server/expressServer.js
+++ server/expressServer.js
@@ -4,58 +4,41 @@ var bp = require('body-parser');
 var exphbs = require('express-handlebars');
 const config = require('./config/config');
 const server_config = config.server_config;
-
 app = express();
 app.use(bp.json());
 app.use('/css', express.static(path.join(__dirname, '..', 'client', 'css')));  // use css files
 app.use('/java', express.static(path.join(__dirname, '..', 'client', 'java')));  // use java files
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-
 const PORT = server_config.port;
 
-// gives the user the signup html page at the domain home path
+// GET Requests
 app.get('/', function(request, response){
     // Send to SSO
     // Check if user has created a profile before
     // If profile exists, redirect to homepage
     response.redirect('/home');
-});
-
-
-
-// GET Requests
-
-/*
+}); // redirect to homepage
 app.get('/signUp', function(request, response){
     response.sendFile(path.join(__dirname, '..', 'client', 'html', 'signUpNew.html'));
-});   // gives the user the signup html page at the domain home path
-*/
-
+});   // shows user the signup html page at the domain home path
 app.get('/home', function(request, response){
-    response.sendFile(path.join(__dirname, '..', 'client', 'html', 'homepage.html'));
-});   // gives user the homepage of Queen's Housing Connect
-
-
+    response.render('home');
+});   // shows user the homepage of Queen's Housing Connect
 app.get('/myProfile', function(request, response){
     response.sendFile(path.join(__dirname, '..', 'client', 'html', 'profile.html'));
 });   // shows the user their private profile
-
 app.get('/housingResources', function(request, response){
     response.sendFile(path.join(__dirname, '..', 'client', 'html', 'housingResources.html'));
 });   // shows user their group information
-
 app.get('/groupStatus', function(request, response){
     response.sendFile(path.join(__dirname, '..', 'client', 'groupStatus.html'));
-
 app.get('/myGroups', function(request, response){
     response.sendFile(path.join(__dirname, '..', 'client','html', 'myGroups.html'));
 });   // shows user their group information
-
 app.get('/profile', function(request, response){
     response.sendFile(path.join(__dirname, '..', 'client', 'html', 'profile.html'));
 });   // shows user another user's public profile
-
 
 
 // POST requests
@@ -77,4 +60,4 @@ app.post('/ModifyGroupInfo', function(request, response){
 
 // start the server
 app.listen(PORT);
-console.log('Listening on port ' + PORT);
+console.log('Server online at port ' + PORT);
